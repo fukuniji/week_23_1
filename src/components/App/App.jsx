2a44b7b72b4f9f7f98228d649fc55b4736bb96d1
fukuniji <marusina.nat@gmail.com>
2023-12-26 00:11:26 +0500

    feat: add card selection on click using useState and spread operator in props

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react"
 import Card from "../Card/Card.jsx"
 import data from "../../data.json"
 
 import style from "./App.module.scss"
 
 function App() {
+  const [selectedCard, setSelectedCard] = useState(3);
+
+  const handleCardSelect = (id) => {
+    setSelectedCard(id);
+  };
+
   return (
     <>
       <div className={style.container}>
         <h1>Тарифы</h1>
         <div className={style.cards_wrapper}>
           {data.map((item)=>(
-            <Card data={item} key={item.id}/>
+            <Card {...item} key={item.id} isSelected={selectedCard === item.id} onSelect={handleCardSelect}/>
           ))}
         </div>
       </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,25 @@
 import style from "./Card.module.scss"
 
-export default function Card({ data }) {
-  console.log(data.class);
+export default function Card({ color, id, value, speed, description, isSelected, onSelect }) {
+
   return (
     <>
-      <div className={`${style.card} ${style[data.color]} ${data.class ? style[data.class] : ""}`}>
+      <div className={`${style.card} ${style[color]} ${isSelected ? style.active : ""}`} onClick={() => onSelect(id)}>
 
         <div className={style.title}>
-          <p>Безлимитный {data.value}</p>
+          <p>Безлимитный {value}</p>
         </div>
         
         <div className={style.price}>
-          <p><span className={style.currency}><sup>руб</sup></span>{data.value}<span className={style.interval}>/мес</span></p>
+          <p><span className={style.currency}><sup>руб</sup></span>{value}<span className={style.interval}>/мес</span></p>
         </div>
 
         <div className={style.speed}>
-          <p>до {data.speed} Мбит/сек</p>
+          <p>до {speed} Мбит/сек</p>
         </div>
 
         <div className={style.description}>
-          <p>{data.description}</p>
+          <p>{description}</p>
         </div>
 
       </div>
